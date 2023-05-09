85c66b06597df65380aac24c5420480378ead7e2
Noman <noman.khan3701@gmail.com>
2023-05-09 18:14:43 +0530

    cleanup

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,9 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       </Head>
+      
       <JobsPage />
+
     </>
   )
 }
