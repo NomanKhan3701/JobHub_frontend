e40d29f93afdc25059ae2e8bab453ce818b9a357
Noman <noman.khan3701@gmail.com>
2023-05-17 19:14:44 +0530

    Second try

--- next.config.js
+++ next.config.js
@@ -4,15 +4,14 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        source: "/(.*)",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
+          {
+            key: "Accept-CH",
+            value: "Sec-CH-UA-Mobile, Sec-CH-UA-Platform, Sec-CH-UA",
+          },
+        ],
+      },
     ]
   }
 }
